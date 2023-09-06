8f2144ea510131dc23b418e51fe3879f496e8620
Abdulloh <abdullajonmamnazirov@gmail.com>
2023-09-06 15:55:06 +0500

    edit: landing page cards

--- src/Constants/landing-cards.js
+++ src/Constants/landing-cards.js
@@ -1,17 +1,17 @@
 export const CARDS = [
   {
     image: "/landing-image-1.png",
-    title: "Some Title",
-    desc: "lorem ipsum dolor sit amet",
+    title: "Hone your skills",
+    desc: "With WiderAI you can gain skills and knowledge",
   },
   {
     image: "/landing-image-2.png",
-    title: "Some Title",
-    desc: "lorem ipsum dolor sit amet",
+    title: "Practice speaking",
+    desc: "Everyday practice speking and become master at it",
   },
   {
     image: "/landing-image-3.png",
-    title: "Some Title",
-    desc: "lorem ipsum dolor sit amet",
+    title: "AI powered",
+    desc: "WiderAI is powered with new ChatGPT and can assess your skills",
   },
 ];
